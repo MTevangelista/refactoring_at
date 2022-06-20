82565784f9290fa571b5a63a235bdcd2d6836d91
Matheus Evangelista <matheus.evangelista@g.globo>
2022-06-19 21:17:05 -0300

    Ensure alarm activated when tire pressure is outside the expected range

--- tire-pressure-monitoring-system-tests/alarm-test.ts
+++ tire-pressure-monitoring-system-tests/alarm-test.ts
@@ -6,11 +6,15 @@ describe('Tyre Pressure Monitoring System', () => {
 
 	describe('Alarm', () => {
 
-		it('foo', () => {
-			const alarm = new Alarm();
-			expect(alarm.isAlarmOn()).eql(false);
-		});
+		context("When the tire pressure is outside the expected range", () => {
+			it("Should have alarm activated", () => {
+				const psiPressureValue = 16
+				const sut = new Alarm(psiPressureValue)
 
+				sut.check()
+				
+				expect(sut.isAlarmOn()).equal(true)
+			})
+		})
 	});
-
 });
